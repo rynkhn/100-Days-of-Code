aa388ede39959f2388aaa6f5b7a15e260e169ce9
Reyan Khan <reyankhan1603@gmail.com>
2025-12-14 03:46:28 +0530

    sixth day

---
+++ Day_6.py
@@ -0,0 +1,4 @@
+''' Nothing here, Angela Yu gave a gaming website to play games. 
+here is the link: 
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+'''
