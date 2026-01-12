0032d7e8d8e9742b5d0a693a4165f688b6183c4d
Reyan Khan <reyankhan1603@gmail.com>
2026-01-12 22:49:34 +0530

    Thirty First Day

---
+++ Day 31/main.py
@@ -0,0 +1,61 @@
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+current_card = {}
+
+
+def next_card():
+    global current_card
+    global flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text = current_card["French"])
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_background, image=card_back_img)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index = False)
+    next_card()
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width = 800, height = 576, bg= BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file = "images/card_front.png")
+card_back_img = PhotoImage(file = "images/card_back.png")
+card_background = canvas.create_image(400, 288, image = card_front_img)
+canvas.grid(row=0, column=0, columnspan=2)
+
+card_title = canvas.create_text(400, 150, text= "",fill = "#000000" ,font= ("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text= "",fill = "#000000" ,font= ("Ariel", 60, "bold"))
+#adding buttons
+cross = PhotoImage(file= "images/wrong.png")
+wrong_button = Button(image=cross, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command= next_card)
+wrong_button.grid(column=0, row=1)
+tick = PhotoImage(file= "images/right.png")
+right_button = Button(image=tick, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=is_known)
+right_button.grid(column=1, row=1)
+
+next_card()
+
+window.mainloop()
